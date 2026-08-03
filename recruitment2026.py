8f1fa0de2a678752c071cfd58a06078750ffbb8c
kieranc-hash <kierancrowley@utexas.edu>
2026-08-03 17:51:54 -0500

    Add files via upload

--- recruitment2026.py
+++ recruitment2026.py
@@ -104,6 +104,20 @@ SportsMediaMinor=['Emerson Pak', 'Kate Hoog', 'Parker Keston']
 UTeachMinor=['Elizabeth Perales', 'Sasha Pittsford']
 WealthManagementMinor=['Hannah Belenkiy', 'Laraleigh Hackerman']
 
+# COLLEGES
+McCombsSchoolofBusiness=['Ava Dahlander', 'Catherine Dooley', 'Celeste Gutierrez', 'Hannah Belenkiy', 'Haylee Martin', 'Kate Belman', 'Katie Corbin', 'Kendall Klimek', 'Laraleigh Hackerman', 'Madison Taylor', 'Marie Jolie Day', 'Natalie Yoder', 'Olivia Alvarez', 'Sharon Yoon']
+CockrellSchoolofEngineering=['Daisy Challoner', 'Elizabeth Porter', 'Katie Corbin', 'Katie Windell', 'Morgan Gaitz', 'Ollie Mae Harrison', 'Siri Pemberton', 'Zeina Saghiyyah']
+CollegeofNaturalSciences=['Andrea Yu', 'Audrey Cooper', 'Audrey Jones', 'Braeleigh Garcia', 'Buckley Turner', 'Caitlin Van Sant', 'Elizabeth Perales', 'Emmerich Benavides', 'Farrah Childs', 'Genesis Martinez', 'Hadley Amann', 'Hannah Dollinger', 'Hasita Karthikeyan', 'Izzy Davies', 'Jennifer Rosado', 'Kimia Forouzesh', 'Lauren Henderson', 'Lee Lloyd', 'Maria Sanchez', 'Megan Garza', 'Mrinali Ganta', 'Neela Juarez', 'Rishona Mopur', 'Sammi Gill', 'Sanya Hoskere', 'Sophia Labazzo', 'Sophie Coe']
+MoodyCollegeofCommunication=['Abrielle Gallini', 'Annabelle Judson', 'Annie Boeh', 'Annie Hoag', 'Brooke Rosenberg', 'Brooke Waldman', 'Delaney O’Brien', 'Elizabeth Scull', 'Ella Garber', 'Elyse Miller', 'Emma Levy', 'Emma Schneidau', 'Emmy Pak', 'Georgia Key', 'Giada Scaramuzza', 'Giana Toothman', 'Jazlyn Mata', 'Kate Belman', 'Kate Flanery', 'Kate Hoog', 'Katie Walsh', 'Kendall Klimek', 'Kirra Vrzal', 'Macie McKie', 'Molly Bogar', 'Parker Keston', 'Tatum Samuels', 'Victoria Olivarez', 'Zoë Westbrook']
+CollegeofLiberalArts=['Addie Brooks', 'Addison Starr', 'Alexa Lira', 'Angie Andersen', 'Annie Boeh', 'Arani Walton', 'Bella Champion', 'Caroline Schulze', 'Charlie Stone', 'Dylan Kaufman', 'Ella Leininger', 'Ema Avila', 'Emma Levy', 'Hannah Reilly', 'Hazel Wells', 'Josie Daik', 'Kaelyn Liu', 'Kaitlin Black', 'Kamryn Lowery', 'Kassidy', 'Lily Robbins', 'Margot Rosenband', 'May Downey', 'Mia Cisneros', 'Paola Castillo', 'Sarah Kester', 'Sasha Pittsford', 'Skye Doughton', 'Sofia Argoti', 'Sofia Bartkoski', 'Sofie Arroyo', 'Sofie Martinez', 'Sophia Sayers', 'Taylor Jennings', 'Zoe Veliz', 'Zoey Bustamante', 'Zoë Westbrook', 'Anabel Precht']
+CollegeofEducation=['Alyssa Garcia', 'Anika Novak', 'Annie Bryce', 'Ella Pitts', 'Sarah Miller', 'Taylor McIlvain', 'Elena Solano', 'Emily Bull']
+CollegeofFineArts=['Amorie Erichsen', 'Ava Hodges', 'Kate Neiman', 'Katelyn Quintanilla', 'Katherine Blumenschein', 'Macie McKie', 'Marie Jolie Day', 'Natalie Juarez', 'Olivia Asay', 'Sofie Combs']
+SchoolofNursing=['Caelyn Prochazka', 'Ema Avila', 'Jasmine Valdez', 'Lexie Hale', 'Maria Sepulveda', 'Ryleigh Montgomery']
+JacksonSchoolofGeosciences=['Christa Hamlin', 'Jacqueline Olivier']
+LBJSchoolofPublicAffairs=['Drew Shelton', 'Madeline Gottemoller']
+SchoolofCivicLeadership=['Ella Dillinger', 'Katie Walsh']
+
+
 # ==== MAPPING INTEREST NAME TO RECRUITER NAME LISTS ===========================================================================================================
 majors = {
         "Accounting": Accounting,
@@ -209,6 +223,21 @@ minors = {
         "Wealth Management Minor": WealthManagementMinor
 }
 
+colleges = {
+    "McCombs Business": McCombsSchoolofBusiness,
+    "Cockrell Engineering": CockrellSchoolofEngineering,
+    "College of Natural Sciences": CollegeofNaturalSciences,
+    "Moody Communication": MoodyCollegeofCommunication,
+    "College of Liberal Arts": CollegeofLiberalArts,
+    "Education": CollegeofEducation,
+    "College of Fine Arts": CollegeofFineArts,
+    "Nursing": SchoolofNursing,
+    "Jackson School of Geosciences": JacksonSchoolofGeosciences,
+    "LBJ School of Public Affairs": LBJSchoolofPublicAffairs,
+    "School of Civic Leadership": SchoolofCivicLeadership,
+}
+
+
 # ===== STREAMLIT USER INTERFACE ===============================================================================================================================
 st.title("Interest Finder")
 
@@ -228,7 +257,7 @@ def checkbox_columns(title, items, num_cols=2):
         
 selected_majors = checkbox_columns("Majors"+'\U0001F4DA', list(majors.keys()), num_cols=4)
 selected_minors = checkbox_columns("Minors"+"\U0001F4DD", list(minors.keys()), num_cols=4)
-#selected_college = checkbox_columns("College/Track"+"\U0001FA7A", list(college.keys()), num_cols=4)
+selected_college = checkbox_columns("College/Track"+"\U0001FA7A", list(college.keys()), num_cols=4)
 #selected_hometowns = checkbox_columns("Hometowns"+"\U0001F3E0", list(hometowns.keys()), num_cols=4)
 #selected_schools = checkbox_columns("High Schools"+"\U0001F3EB", list(schools.keys()), num_cols=4)
 #selected_extras = checkbox_columns("HS Extracurriculars"+"\U0001F483", list(extras.keys()), num_cols=4)
@@ -238,7 +267,7 @@ selected_minors = checkbox_columns("Minors"+"\U0001F4DD", list(minors.keys()), n
 #selected_nicheinterests = checkbox_columns("Niche Interests"+"\U0001F388", list(nicheinterests.keys()), num_cols=4)
 #selected_transfers = checkbox_columns("Transfer Students"+"\U0001F501", list(transfers.keys()), num_cols=4)
 
-selected_interests = selected_majors + selected_minors #+ selected_college + selected_hometowns + selected_schools + selected_extras + selected_orgs + selected_activities + selected_summercamps + selected_nicheinterests + selected_transfers
+selected_interests = selected_majors + selected_minors + selected_college #+ selected_hometowns + selected_schools + selected_extras + selected_orgs + selected_activities + selected_summercamps + selected_nicheinterests + selected_transfers
 
 # ===== Matching Logic =====
 if selected_interests:
@@ -251,8 +280,8 @@ if selected_interests:
             names = majors[interest]
         elif interest in minors:
             names = minors[interest]
-        #elif interest in college:
-         #   names = college[interest]
+        elif interest in college:
+            names = college[interest]
         #elif interest in hometowns:
          #   names = hometowns[interest]
         #elif interest in schools:
